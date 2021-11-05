b26a28d55ef24b551b5e9ca65fe3179a8f7a19dc
zorbyte <39759284+zorbyte@users.noreply.github.com>
2021-11-05 11:38:52 +1000

    change application to the new union ns type

--- resources/application.ts
+++ resources/application.ts
@@ -3,6 +3,28 @@ import { Nullable } from "../_internals/utils.ts";
 import { Team } from "../models/team.ts";
 import { User } from "./user.ts";
 
+export interface Application {
+  id: Snowflake;
+  name: string;
+  icon: Nullable<string>;
+  description: string;
+  rpc_origins?: string[];
+  bot_public: boolean;
+  bot_require_code_grant: boolean;
+  terms_of_service_url?: string;
+  privacy_policy_url?: string;
+  owner?: User.Partial;
+  summary: string;
+  verify_key: string;
+  team?: Nullable<Team.Team>;
+  guild_id?: Snowflake;
+  primary_sku_id?: Snowflake;
+  slug?: string;
+  cover_image?: string;
+  flags?: Application.Flags | number;
+}
+
+
 export namespace Application {
   export const enum Flags {
     GatewayPresence = 1 << 12,
@@ -14,27 +36,4 @@ export namespace Application {
     GatewayMessageContent = 1 << 18,
     GatewayMessageContentLimited = 1 << 19,
   }
-
-  export interface Application {
-    id: Snowflake;
-    name: string;
-    icon: Nullable<string>;
-    description: string;
-    rpc_origins?: string[];
-    bot_public: boolean;
-    bot_require_code_grant: boolean;
-    terms_of_service_url?: string;
-    privacy_policy_url?: string;
-    owner?: User;
-    summary: string;
-    verify_key: string;
-    team?: Nullable<Team.Team>;
-    guild_id?: Snowflake;
-    primary_sku_id?: Snowflake;
-    slug?: string;
-    cover_image?: string;
-    flags?: Flags | number;
-  }
-
-  export type User = User.Partial;
 }
