e872ba1d4f7fbc2c9a32ed6a61b2f734226a79ce
zorbyte <39759284+zorbyte@users.noreply.github.com>
2021-11-14 23:15:19 +1000

    feat(permissions): wip, starting work on perm flags

--- permissions/permissions.ts
+++ permissions/permissions.ts
@@ -1,10 +1,34 @@
-// This is going to be as painful as Audit Logs... isn't it...
+export type Permission = `${PermissionFlags | number}`;
 
-export namespace Permissions {
-  // deno-lint-ignore no-empty-enum
-  export const enum Flags {
-    // TODO(@zorbyte): Write all of these.
-  }
+export namespace Permission {
+  /** Allows creation of instant invites. - Channel types: T, V, S. */
+  export type CreateInstantInvite = 0x0000000001;
 
-  export type Raw = `${Flags}`;
+  /** Allows creation of instant invites. */
+  export const CreateInstantInvite = 1 << 0;
+
+  /* Allows kicking members. */
+  export type KickMembers = 0x0000000002;
+
+  /* Allows kicking members. */
+  export const KickMembers = 1 << 1;
+
+  /** Allows banning members. */
+  export type BanMembers = 0x0000000004;
+
+  /** Allows banning members. */
+  export const BanMembers = 1 << 2;
+
+  /** Allows all permissions and bypasses channel permission overwrites. */
+  export type Administrator = 0x0000000008;
+
+  /** Allows all permissions and bypasses channel permission overwrites. */
+  export const Administrator = (1 << 3);
+
+  
 }
+
+/** https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags */
+export type PermissionFlags =
+  | Permission.CreateInstantInvite
+  | Permission.KickMembers;
