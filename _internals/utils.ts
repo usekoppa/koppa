235b88ae8e1bd7fe54c3a6e116a5c94ac9640852
zorbyte <39759284+zorbyte@users.noreply.github.com>
2021-11-16 14:32:22 +1000

    lint: utils

--- _internals/utils.ts
+++ _internals/utils.ts
@@ -1,8 +1,7 @@
-export type Nullable<T> = T | null;
-
-export type ZeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-
 // deno-lint-ignore no-explicit-any
 export type $TODO<Temp = any> = Temp;
 
+export type Nullable<T> = T | null;
 export type NonNullableObject<T> = { [P in keyof T]?: NonNullable<T[P]> };
+
+export type ZeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
